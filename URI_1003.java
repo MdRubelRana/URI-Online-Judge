e10087190f4e4df62d9262bf4712880232551e38
Md Rubel Rana <33339942+MdRubelRana@users.noreply.github.com>
2017-11-25 11:31:09 +0600

    Update URI_1003.java

--- URI_1003.java
+++ URI_1003.java
@@ -4,7 +4,7 @@ public class URI_1003 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int a, b;
-		System.out.println("enter: ");
+		
 		a = sc.nextInt();
 		b = sc.nextInt();
 		
